8b4e0eb7e030a62f447c8bdb4dc28d84ba8031f4
Kevin Kibbz <kibebekevin@gmail.com>
2023-11-18 15:29:40 +0300

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,12 +114,19 @@ ASGI_APPLICATION = 'portfolio.asgi.application'
 
 if DEBUG:
 
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+    
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+        'default': dj_database_url.config(
+            default=env('DATABSE_URL'),
+            conn_max_age=600, 
+        )
+	}
     
 else:
 	DATABASES = {
